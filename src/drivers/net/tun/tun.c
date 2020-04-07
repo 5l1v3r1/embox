459a99f68df7bf2217e8e835fe30f7c5ba3e513f
Anton Bondarev <anton.bondarev2310@gmail.com>
2020-04-07 14:36:09 +0300

    tun

--- src/drivers/net/tun/tun.c
+++ src/drivers/net/tun/tun.c
@@ -81,10 +81,13 @@ static int tun_set_mac(struct net_device *dev, const void *addr) {
 static int tun_xmit(struct net_device *dev, struct sk_buff *skb) {
 	struct tun *tun = netdev_priv(dev);
 	struct ethhdr *ethh;
-	ethh = skb->mac.ethh;
+
+	/* we don't build headers for dev with NOARP flag */
+	ethh = eth_hdr(skb);
 	ethh->h_proto = htons(ETH_P_IP);
 	memcpy(ethh->h_source, skb->dev->dev_addr, ETH_ALEN);
 	memset(ethh->h_dest, 0, ETH_ALEN);
+
 	skb_queue_push(&tun->rx_q, skb);
 	waitq_wakeup(&tun->wq, 1);
 	return 0;
@@ -235,6 +238,7 @@ static ssize_t tun_dev_write(struct idesc *idesc, const struct iovec *iov, int c
 	int ret = 0;
 	size_t size = 0;
 	unsigned char *raw;
+	struct ethhdr *ethh;
 
 	err = tun_netdev_by_idesc(idesc, &netdev, &tun);
 	if (err) {
@@ -253,14 +257,12 @@ static ssize_t tun_dev_write(struct idesc *idesc, const struct iovec *iov, int c
 	if (!skb) {
 		return -ENOMEM;
 	}
-#if 0
-	struct ethhdr *ethh;
-	ethh = skb->mac.ethh;
+
+	ethh = eth_hdr(skb);
+
 	ethh->h_proto = htons(ETH_P_IP);
 	memcpy(ethh->h_dest, netdev->dev_addr, ETH_ALEN);
 	memset(ethh->h_source, 0, ETH_ALEN);
-#endif
-	ethhdr_build(skb->mac.ethh, netdev->dev_addr, NULL, ETH_P_IP);
 	
 	raw = skb->mac.raw + ETH_HLEN;
 	for (int i = 0; i < cnt; ++i) {
